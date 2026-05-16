c4a546b3c7cc7a632f87bbbcc159c4363b569ed3
Dinesh <dineshkarthie@gmail.com>
2026-05-16 13:54:04 +0530

    Version Bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.app.exoplanethunter"
         minSdk = libs.versions.min.sdk.get().toInt()
         targetSdk = libs.versions.target.sdk.get().toInt()
-        versionCode = 19
-        versionName = "1.12.6"
+        versionCode = 20
+        versionName = "1.12.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
